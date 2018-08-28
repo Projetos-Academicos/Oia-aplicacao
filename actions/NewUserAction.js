3e1282e0c3720617bc8f0aba04de46ea21cd4818
Jefferson-random <jeffersoneuclides83@gmail.com>
2018-08-28 00:34:13 -0300

    adicionando usuário no firebase, faltando polimento

--- actions/NewUserAction.js
+++ actions/NewUserAction.js
@@ -3,5 +3,18 @@ import firebase from 'firebase';
 export const tryRegister = ({email, password}) => dispatch => {
     return firebase
         .auth()
-        .createUserWithEmailAndPassword(email, password);
+        .createUserWithEmailAndPassword(email, password)
+            .catch((error) => {
+                alert(error.code);
+            });
+}
+
+export const tryInsert = ({email, name, lastName, birthDate, user}) => dispatch => {
+   
+    firebase.database().ref('user').child(user.uid).set({
+        name: name,
+        last_name: lastName,
+        birth_date: birthDate,
+        email: email
+    });
 }
