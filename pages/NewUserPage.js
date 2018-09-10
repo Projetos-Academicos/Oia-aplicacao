19079ddd2aee0ce855a63e27ba0c4fa2310e6999
Jefferson-random <jeffersoneuclides83@gmail.com>
2018-09-10 00:03:22 -0300

    retirado o redux, faltando detalhes

--- pages/NewUserPage.js
+++ pages/NewUserPage.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
-import { connect } from 'react-redux';
+import { StyleSheet, Text, View, TextInput, Button, ScrollView, ActivityIndicator } from 'react-native';
+//import { connect } from 'react-redux';
+import firebase from 'firebase';
+import { messageErroCodeNewUser } from "../utils";
 
 import { FormRow } from '../components';
-import { tryRegister, tryInsert } from "../actions";
+//import { tryRegister, tryInsert } from "../actions";
 
-class NewUserPage extends React.Component {
+export default class NewUserPage extends React.Component {
     constructor(props) {
         super(props);
 
@@ -14,24 +16,96 @@ class NewUserPage extends React.Component {
             msgError: "",
             email: "",
             password: "",
+            passwordConfirm: "",
             birthDate: "",
             name: "",
             lastName: ""
         }
 
-    } 
-    
-    changeTextInput(chave, valor){
+    }
+
+    tryRegister({ email, password }) {
+        return firebase
+            .auth()
+            .createUserWithEmailAndPassword(email, password)
+            .catch((error) => {
+                this.setState({ message: messageErroCodeNewUser(error.code) })
+            });
+    }
+
+    tryInsert({ email, name, lastName, birthDate }) {
+
+        firebase.auth().signOut();
+
+        firebase.auth().onAuthStateChanged((user) => {
+            if (user) {
+                firebase.database().ref('user').child(user.uid).set({
+                    name: name,
+                    last_name: lastName,
+                    birth_date: birthDate,
+                    email: email
+                })
+                .catch((error) => {
+                    this.setState({ message: messageErroCodeNewUser(error.code) })
+                });
+                Alert.alert("Bem-Vindo " + user.name + "!", "Registro efetuado com sucesso!");
+                this.props.navigation.replace("HomePage");
+            }
+        })
+    }
+
+    changeTextInput(chave, valor) {
         this.setState({
             [chave]: valor
-        });      
-    } 
-    
-    register(){
-        const {email, password, name, lastName, birthDate} = this.state;
-        
-        this.props.tryRegister({email, password});
-        this.props.tryInsert({email, name, lastName, birthDate})
+        });
+    }
+
+    register() {
+        const { email, password, name, lastName, birthDate } = this.state;
+
+        if (this.passwordConfirmed()) {
+            this.tryRegister({ email, password })
+                .then(
+                    this.tryInsert({ email, name, lastName, birthDate })
+                );
+        } else {
+            this.setState({ message: "Senhas não conferem" });
+        }
+    }
+
+    passwordConfirmed() {
+        const { password, passwordConfirm } = this.state;
+
+        if (password === passwordConfirm) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    renderMessage() {
+        const { message } = this.state;
+        if (!message) {
+            return null;
+        }
+        return (
+            <View style={styles.errorMessage}>
+                <Text>{message}</Text>
+            </View>
+        );
+    }
+
+    renderButton() {
+        if (this.state.isLoading) {
+            return <ActivityIndicator />;
+        }
+        return (
+            <View style={styles.containerButton}>
+                <Button title="Cadastrar" onPress={() => {
+                    this.register()
+                }} />
+            </View>
+        );
     }
 
     render() {
@@ -42,81 +116,80 @@ class NewUserPage extends React.Component {
                 </View>
                 <View style={styles.containerPage}>
                     <View style={styles.containerLogin}>
-                        <FormRow first>
-                            <Text style = {styles.label}>Email</Text>
-                            <TextInput
-                                style={styles.input}
-                                placeholder="exemplo@exemplo.com"
-                                underlineColorAndroid='transparent'
-                                value={this.state.email}
-                                onChangeText={value => {this.changeTextInput('email', value)}}
+                        <ScrollView>
+                            <FormRow first>
+                                <Text style={styles.label}>Email</Text>
+                                <TextInput
+                                    style={styles.input}
+                                    placeholder="exemplo@exemplo.com"
+                                    underlineColorAndroid='transparent'
+                                    value={this.state.email}
+                                    onChangeText={value => { this.changeTextInput('email', value) }}
 
-                            />
-                        </FormRow>
-                        <FormRow>
-                            <Text style = {styles.label}>Senha</Text>
-                            <TextInput
-                                style={styles.input}
-                                placeholder="******"
-                                underlineColorAndroid='transparent'
-                                secureTextEntry
-                                value={this.state.password}
-                                onChangeText={value => { this.changeTextInput('password', value)}}
-                            />
-                        </FormRow>
+                                />
+                            </FormRow>
+                            <FormRow>
+                                <Text style={styles.label}>Senha</Text>
+                                <TextInput
+                                    style={styles.input}
+                                    placeholder="******"
+                                    underlineColorAndroid='transparent'
+                                    secureTextEntry
+                                    value={this.state.password}
+                                    onChangeText={value => { this.changeTextInput('password', value) }}
+                                />
+                            </FormRow>
 
-                        <FormRow>
-                            <Text style = {styles.label}>Confirme a senha</Text>
-                            <TextInput
-                                style={styles.input}
-                                placeholder="******"
-                                underlineColorAndroid='transparent'
-                                secureTextEntry
-                                value={this.state.password}
-                                onChangeText={value => { this.changeTextInput('password', value)}}
-                            />
-                        </FormRow>
+                            <FormRow>
+                                <Text style={styles.label}>Confirme a senha</Text>
+                                <TextInput
+                                    style={styles.input}
+                                    placeholder="******"
+                                    underlineColorAndroid='transparent'
+                                    secureTextEntry
+                                    value={this.state.passwordConfirm}
+                                    onChangeText={value => { this.changeTextInput('passwordConfirm', value) }}
+                                />
+                            </FormRow>
 
-                        <FormRow>
-                            <Text style = {styles.label}>Nome</Text>
-                            <TextInput
-                                style={styles.input}
-                                placeholder="Digite seu nome"
-                                underlineColorAndroid='transparent'
-                                value={this.state.name}
-                                onChangeText={value => { this.changeTextInput('name', value)}}
-                            />
-                        </FormRow>
+                            <FormRow>
+                                <Text style={styles.label}>Nome</Text>
+                                <TextInput
+                                    style={styles.input}
+                                    placeholder="Digite seu nome"
+                                    underlineColorAndroid='transparent'
+                                    value={this.state.name}
+                                    onChangeText={value => { this.changeTextInput('name', value) }}
+                                />
+                            </FormRow>
 
-                        <FormRow>
-                            <Text style = {styles.label}>Sobrenome</Text>
-                            <TextInput
-                                style={styles.input}
-                                placeholder="Digite seu sobrenome"
-                                underlineColorAndroid='transparent'
-                                value={this.state.lastName}
-                                onChangeText={value => { this.changeTextInput('lastName', value)}}
-                            />
-                        </FormRow>
+                            <FormRow>
+                                <Text style={styles.label}>Sobrenome</Text>
+                                <TextInput
+                                    style={styles.input}
+                                    placeholder="Digite seu sobrenome"
+                                    underlineColorAndroid='transparent'
+                                    value={this.state.lastName}
+                                    onChangeText={value => { this.changeTextInput('lastName', value) }}
+                                />
+                            </FormRow>
 
-                        <FormRow>
-                            <Text style = {styles.label}>Data de Nascimento</Text>
-                            <TextInput
-                                style={styles.input}
-                                placeholder="DD/MM/AAAA"
-                                underlineColorAndroid='transparent'
-                                value={this.state.birthDate}
-                                onChangeText={value => { this.changeTextInput('birthDate', value)}}
-                            />
-                        </FormRow>
+                            <FormRow>
+                                <Text style={styles.label}>Data de Nascimento</Text>
+                                <TextInput
+                                    style={styles.input}
+                                    placeholder="DD/MM/AAAA"
+                                    underlineColorAndroid='transparent'
+                                    value={this.state.birthDate}
+                                    onChangeText={value => { this.changeTextInput('birthDate', value) }}
+                                />
+                            </FormRow>
 
-                        <FormRow last>
-                            <View style={styles.containerButton}>
-                                <Button title="Cadastrar" onPress={() => {
-                                    this.register() 
-                                }} />
-                            </View>
-                        </FormRow>
+                            <FormRow last>
+                                {this.renderButton()}
+                                {this.renderMessage()}
+                            </FormRow>
+                        </ScrollView>
                     </View>
                 </View>
             </View>
@@ -129,6 +202,7 @@ const styles = StyleSheet.create({
 
     containerPage: {
         flexDirection: 'row',
+        backgroundColor: '#C6C6C6'
     },
     containerLogin: {
         flex: 1,
@@ -143,7 +217,7 @@ const styles = StyleSheet.create({
         paddingLeft: 5,
         paddingRight: 5,
         paddingBottom: 5,
-        backgroundColor: "#dcdcdc",
+        backgroundColor: "#FFF",
         textAlign: 'center',
         borderColor: "transparent",
         borderWidth: 1,
@@ -161,12 +235,16 @@ const styles = StyleSheet.create({
     },
     lbLogin: {
         fontSize: 30,
-        margin: 5
+        margin: 5,
     },
     label: {
         marginLeft: 5,
         fontSize: 15,
+        color: '#fff'
+    },
+    errorMessage: {
+        fontSize: 15,
+        alignSelf: 'center',
+        color: 'red'
     }
 });
-
-export default connect(null, {tryRegister, tryInsert})(NewUserPage);
