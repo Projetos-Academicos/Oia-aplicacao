209ced18860b03d6509855b69c9b913d5d4b6123
Ricardo Lima <ricardoccofg@gmail.com>
2018-11-18 17:57:25 -0300

    mascara do prazo

--- components/ContainerDetails.js
+++ components/ContainerDetails.js
@@ -8,43 +8,43 @@ const ContainerDetails = props => {
 
   function candidatarVaga() {
     axios({
-        method: 'post',
-        url: 'https://oia-api.herokuapp.com/admin/candidatar-se/' + 2 + '/' + vaga.id})
-        .then(function (response) {
-          console.log('funcionou');
-            console.log(response.data);
-        })
-        .catch(function (error) {
-            console.log(error);
-        })
+      method: 'post',
+      url: 'https://oia-api.herokuapp.com/admin/candidatar-se/' + 2 + '/' + vaga.id
+    })
+      .then(function (response) {
+        console.log('funcionou');
+        console.log(response.data);
+      })
+      .catch(function (error) {
+        console.log(error);
+      })
   }
 
   return (
     <ScrollView>
-     <View style={styles.container}>
+      <View style={styles.container}>
         <View style={styles.dadosTopo}>
           <View style={[styles.row, styles.separete]}>
             <Text style={[styles.dados, styles.text, styles.title]}>{vaga.titulo}</Text>
-            <Icon name="content-cut" size={24} color='#808080' />
           </View>
 
           <View style={styles.row}>
-            <Icon name='map-marker' size={22} color='#E75A4D'/>
+            <Icon name='map-marker' size={22} color='#E75A4D' />
             <Text style={[styles.dados, styles.text]}>{vaga.cidade.nome}</Text>
           </View>
 
           <View style={styles.row}>
-            <Icon name='calendar-range' size={22} color='black'/>
-            <Text style={styles.text}>{vaga.prazo}</Text>
+            <Icon name='calendar-range' size={22} color='black' />
+            <Text>
+              <Text style={styles.text}>{vaga.prazo}</Text>
+              <Text style={styles.text}>{vaga.prazo > 1 ? ' dias' : ' dia'}</Text>
+            </Text>
           </View>
         </View>
 
         <View style={styles.description}>
           <View style={[styles.row, styles.separete]}>
             <Text style={styles.descriptionTitle}>Descrição</Text>
-            <View style={styles.prazo}>
-              <Text style={styles.time}>{vaga.prazo}</Text>
-            </View>
           </View>
         </View>
 
@@ -58,116 +58,116 @@ const ContainerDetails = props => {
           </View>
         </View>
 
-      <View style={styles.footer}>
-        <TouchableOpacity style={styles.button} onPress={() => { this.candidatarVaga()}}>
-          <Text style={{color: 'white', fontSize: 18, fontWeight: 'bold'}}> QUERO ESSA OIA </Text>
-        </TouchableOpacity>
+        <View style={styles.footer}>
+          <TouchableOpacity style={styles.button} onPress={() => { this.candidatarVaga() }}>
+            <Text style={{ color: 'white', fontSize: 18, fontWeight: 'bold' }}> QUERO ESSA OIA </Text>
+          </TouchableOpacity>
         </View>
-     </View>
-     </ScrollView>
+      </View>
+    </ScrollView>
   );
 }
 
 export default ContainerDetails;
 
 const styles = StyleSheet.create({
- container: {
-   flex: 1,
-   backgroundColor: "#fff"
- },
- dadosTopo: {
-   marginTop: 15,
-   marginHorizontal: 5,
-   paddingLeft: 5,
- },
+  container: {
+    flex: 1,
+    backgroundColor: "#fff"
+  },
+  dadosTopo: {
+    marginTop: 15,
+    marginHorizontal: 5,
+    paddingLeft: 5,
+  },
 
 
- row: {
-  flexDirection: "row",
-  borderBottomWidth: 1,
-  borderColor: "#DEDEDE",
-  marginBottom: 5
- },
+  row: {
+    flexDirection: "row",
+    borderBottomWidth: 1,
+    borderColor: "#DEDEDE",
+    marginBottom: 5
+  },
 
- separete: {
-  justifyContent: "space-between"
- },
- dados: {
-   paddingBottom: 5
- },
-
-
- title: {
-  fontWeight: "bold",
-  fontSize: 23,
-  color: "#000",
- },
- text: {
-   fontSize: 20,
-   color: '#AFAFAF',
- },
+  separete: {
+    justifyContent: "space-between"
+  },
+  dados: {
+    paddingBottom: 5
+  },
 
 
- description: {
-   marginTop: 15,
-   marginHorizontal: 5,
-   paddingLeft: 5,
- },
- descriptionTitle: {
-   fontSize: 23,
-   fontWeight: "bold",
- },
- prazo: {
-  padding: 3,
-  backgroundColor: "#6F2B8F"
- },
- time: {
-  color: '#fff',
-  fontSize: 20
- },
+  title: {
+    fontWeight: "bold",
+    fontSize: 23,
+    color: "#000",
+  },
+  text: {
+    fontSize: 20,
+    color: '#AFAFAF',
+  },
 
 
- textDescription: {
-  marginTop: 10,
-  marginHorizontal: 5,
-  paddingLeft: 5,
-  borderBottomWidth: 1,
-  borderColor: "#DEDEDE",
- },
- descricao: {
-  fontSize: 16,
-  color: '#AFAFAF',
- },
+  description: {
+    marginTop: 15,
+    marginHorizontal: 5,
+    paddingLeft: 5,
+  },
+  descriptionTitle: {
+    fontSize: 23,
+    fontWeight: "bold",
+  },
+  prazo: {
+    padding: 3,
+    backgroundColor: "#6F2B8F"
+  },
+  time: {
+    color: '#fff',
+    fontSize: 20
+  },
 
- valor: {
-   marginHorizontal: 5,
-   paddingLeft: 5,
-   marginBottom: 10,
-   flexDirection: 'row',
-   justifyContent: 'flex-end'
- },
 
- divValor: {
-  backgroundColor: "#22B34E",
-  padding: 5,
- },
+  textDescription: {
+    marginTop: 10,
+    marginHorizontal: 5,
+    paddingLeft: 5,
+    borderBottomWidth: 1,
+    borderColor: "#DEDEDE",
+  },
+  descricao: {
+    fontSize: 16,
+    color: '#AFAFAF',
+  },
 
- textValor: {
-   fontSize: 22,
-   fontWeight: "bold",
-   color: "white"
- },
+  valor: {
+    marginHorizontal: 5,
+    paddingLeft: 5,
+    marginBottom: 10,
+    flexDirection: 'row',
+    justifyContent: 'flex-end'
+  },
 
- footer: { 
-   flex: 1,
-   backgroundColor: "#fff"
- },
+  divValor: {
+    backgroundColor: "#22B34E",
+    padding: 5,
+  },
 
- button: {
-  margin: 5,
-  alignItems: 'center',
-  elevation: 3,
-  backgroundColor: '#22B34E',
-  padding: 15,
-},
+  textValor: {
+    fontSize: 22,
+    fontWeight: "bold",
+    color: "white"
+  },
+
+  footer: {
+    flex: 1,
+    backgroundColor: "#fff"
+  },
+
+  button: {
+    margin: 5,
+    alignItems: 'center',
+    elevation: 3,
+    backgroundColor: '#22B34E',
+    padding: 15,
+  },
 });
